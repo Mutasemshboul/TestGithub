3c614a3708d89785d9a7cde80019a8ae3cc1ffd4
MutasemShboul <mutasemshboul14@gmail.com>
2022-06-29 09:44:14 +0300

    added new

---
+++ learn.infra/Dbcontext.cs
@@ -0,0 +1,40 @@
+﻿using learn.core.domain;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace learn.infra
+{
+    
+    public class Dbcontext : IDBContext
+    {
+        private DbConnection connection;
+        private IConfiguration configuration;
+        public Dbcontext(DbConnection connection, IConfiguration configuration)
+        {
+            this.connection = connection;
+            this.configuration = configuration;
+        }
+        public DbConnection dBConnection
+        {
+            get
+            {
+                if (connection == null)
+                {
+
+                    connection = new OracleConnection(configuration["ConnectionStrings:DBConnectionString"]);
+
+                    connection.Open();
+                }
+                else if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                return connection;
+            }
+        }
+    }
+}
